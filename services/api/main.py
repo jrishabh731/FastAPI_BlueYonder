52e1b182af7c10068eda2ae86149042bc2195821
rvjain <rvjain@parallelwireless.com>
2022-06-30 16:39:35 +0530

    First Commit for BlueYonder Repo

---
+++ services/api/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.get("/get_record/{appointment_id}")
+def create_file(appointment_id):
+    return {"ID": appointment_id}
+
+
+@app.post("/upload_data/")
+def create_upload_file(file: UploadFile):
+    return {"status": 200}
