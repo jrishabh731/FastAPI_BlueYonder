611391af75d560429622037d054052699866c12a
rvjain <rvjain@parallelwireless.com>
2022-06-30 17:22:40 +0530

    Added changes

--- services/api/main.py
+++ services/api/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 
+
+
 app = FastAPI()
 
 
@@ -10,4 +12,6 @@ def create_file(appointment_id):
 
 @app.post("/upload_data/")
 def create_upload_file(file: UploadFile):
-    return {"status": 200}
+    return {"status": 200,
+            "filename": file.filename,
+            "data": file.read()}
