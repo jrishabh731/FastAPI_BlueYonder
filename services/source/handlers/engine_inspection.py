0c507f1e6f763cad03d08bb7c38a15c1cbc3e81a
rvjain <rvjain@parallelwireless.com>
2022-07-03 23:29:42 +0530

    Added changes till no restructuring

--- services/source/handlers/engine_inspection.py
+++ services/source/handlers/engine_inspection.py
@@ -2,7 +2,6 @@ import csv
 import datetime
 from io import StringIO
 
-# from source.config.db import get_db_session
 from source.models.EngineInspectionSchema import EngineInspectionData
 from source.schema.EnginePydantic import EngineInspectionSchema
 from sqlalchemy.orm import Session
@@ -11,13 +10,8 @@ from source.config.db import engine
 
 def get_engine_inspection(id):
     try:
-        print(f"session local is not created")
         record = []
-        SessionLocal = get_db_session()
-        import pdb
-        pdb.set_trace()
         with Session(bind=engine, expire_on_commit=False) as session:
-            print(f"session local is created, inside context manager")
             record = session.query(EngineInspectionData).get(id)
             if record is None:
                 record = []
@@ -27,22 +21,21 @@ def get_engine_inspection(id):
 
 
 def post_engine_inspection(filedata):
+    response = {
+        "duplicate_records": [],
+        "schema_validations": [],
+        "metadata": {
+            "inserted_records": 0
+        }
+    }
     try:
         results = csv.DictReader(StringIO(str(filedata, 'utf-8-sig')), delimiter=',')
-        response = {
-            "duplicate_records": [],
-            "schema_validations": [],
-            "metadata": {
-                "inserted_records": 0
-            }
-        }
 
         appointment_obj = {}
         cnt = 0
         for result in results:
             if cnt == 10000:
                 break
-            print(cnt)
             cnt += 1
             res = dict(result)
             res["inspectionDate"] = res.pop("inspection date", None)
@@ -68,20 +61,14 @@ def post_engine_inspection(filedata):
                     response["duplicate_records"].append(error_resp)
                     continue
                 appointment_obj[obj.appointmentId] = EngineInspectionData(**res)
-        SessionLocal = get_db_session()
-        print(f"Keys : {list(appointment_obj.keys())}")
-        import pdb
-        pdb.set_trace()
+
         with Session(bind=engine, expire_on_commit=False) as session:
-            print(dir(session))
             records = session.query(EngineInspectionData) \
                 .with_entities(EngineInspectionData.appointmentId) \
                 .filter(EngineInspectionData.appointmentId.in_(list(appointment_obj.keys())))
-        records = [record for record in records]
-        print(f"Records from filter: {records}")
+
         for record in records:
             try:
-                print(f"Records : {record}")
                 if appointment_obj.pop(record.appointmentId, None):
                     error_resp = {
                         "message": f'{record.appointmentId} is already present in DB.',
@@ -91,7 +78,6 @@ def post_engine_inspection(filedata):
             except Exception as err:
                 print(f"Exception: {err}")
         if appointment_obj:
-            SessionLocal = get_db_session()
             with Session(bind=engine, expire_on_commit=False) as session:
                 session.add_all(appointment_obj.values())
                 try:
@@ -103,5 +89,4 @@ def post_engine_inspection(filedata):
             print("No records left to insert.")
     except Exception as err:
         print(f"Error occured: {err}")
-    print("Response returned")
     return response
--- services/source/main.py
+++ services/source/main.py
@@ -4,10 +4,7 @@ from io import StringIO
 
 from fastapi import FastAPI, status, UploadFile, File
 from source.config.db import Base, engine
-from source.models.EngineInspectionSchema import EngineInspectionData
-from sqlalchemy.orm import Session
-from source.schema.EnginePydantic import EngineInspectionSchema
-from source.handlers.engine_inspection import get_engine_inspection
+from source.handlers.engine_inspection import get_engine_inspection, post_engine_inspection
 
 Base.metadata.create_all(engine)
 
@@ -17,92 +14,85 @@ app = FastAPI()
 
 @app.get("/get_record", status_code=200)
 def get_inspection_details(id: str):
-    id = f"aj_{id}"
-    import pdb
-    # pdb.set_trace()
-    with Session(bind=engine, expire_on_commit=False) as session:
-        record = session.query(EngineInspectionData).get(id)
-        if record is None:
-            record = []
-
-    return {"record": record}
+    return get_engine_inspection(f"aj_{id}")
 
 
 @app.post("/upload_file/", status_code=status.HTTP_200_OK)
 async def upload_file(file: UploadFile=File(default=None, media_type="multipart/form-data")):
     data = await file.read()
-    str_data = StringIO(str(data, 'utf-8-sig'))
-    results = csv.DictReader(str_data, delimiter=',')
-    response = {
-        "duplicate_records": [],
-        "schema_validations": [],
-        "metadata": {
-            "inserted_records": 0
-        }
-    }
-
-    appointment_ids = []
-    appointment_obj = []
-    cnt = 0
-    for result in results:
-        if cnt == 10000:
-            break
-        cnt += 1
-        res = dict(result)
-        res["inspectionDate"] = res.pop("inspection date", None)
-        dt_inspectionDate = datetime.datetime.strptime(res["inspectionDate"], "%d-%m-%Y")
-        res["inspectionDate"] = dt_inspectionDate.strftime("%m-%d-%Y")
-        res["inspectionStartTime"] = res.pop("inspection time", "").split(" ")[-1]
-        try:
-            obj = EngineInspectionSchema(**res)
-
-        except Exception as err:
-            print(f"Error found : {err}")
-            print(res)
-            error_resp = {
-                "error": str(err),
-                "appointmentId": res["appointmentId"],
-                "message": "Schema creation failed for this appointmentID."
-            }
-            response["schema_validations"].append(error_resp)
-        else:
-            if obj.appointmentId in appointment_ids:
-                print(f'{obj.appointmentId} is duplicate in the current file, ignoring it.')
-                error_resp = {
-                    "message": f'{obj.appointmentId} is duplicate in the file',
-                    "appointmentId": res["appointmentId"],
-                }
-                response["duplicate_records"].append(error_resp)
-                continue
-            appointment_ids.append(obj.appointmentId)
-            appointment_obj.append(EngineInspectionData(**res))
-
-
-    with Session(bind=engine, expire_on_commit=False) as session:
-        records = session.query(EngineInspectionData)\
-            .with_entities(EngineInspectionData.appointmentId)\
-            .filter(EngineInspectionData.appointmentId.in_(appointment_ids))
-    for record in records:
-        try:
-            indx = appointment_ids.index(record.appointmentId)
-            appointment_ids.pop(indx)
-            appointment_obj.pop(indx)
-            error_resp = {
-                "message": f'{record.appointmentId} is already present in DB.',
-                "appointmentId": record.appointmentId
-            }
-            response["duplicate_records"].append(error_resp)
-        except Exception as err:
-            print(f"Exception: {err}")
-    if appointment_obj:
-        with Session(bind=engine, expire_on_commit=False) as session:
-            session.add_all(appointment_obj)
-            try:
-                session.commit()
-                response["metadata"]["inserted_records"] = len(appointment_obj)
-            except Exception as err:
-                pass
-    else:
-        print("No records left to insert.")
-
-    return response
+    return post_engine_inspection(data)
+    # str_data = StringIO(str(data, 'utf-8-sig'))
+    # results = csv.DictReader(str_data, delimiter=',')
+    # response = {
+    #     "duplicate_records": [],
+    #     "schema_validations": [],
+    #     "metadata": {
+    #         "inserted_records": 0
+    #     }
+    # }
+    #
+    # appointment_ids = []
+    # appointment_obj = []
+    # cnt = 0
+    # for result in results:
+    #     if cnt == 10000:
+    #         break
+    #     cnt += 1
+    #     res = dict(result)
+    #     res["inspectionDate"] = res.pop("inspection date", None)
+    #     dt_inspectionDate = datetime.datetime.strptime(res["inspectionDate"], "%d-%m-%Y")
+    #     res["inspectionDate"] = dt_inspectionDate.strftime("%m-%d-%Y")
+    #     res["inspectionStartTime"] = res.pop("inspection time", "").split(" ")[-1]
+    #     try:
+    #         obj = EngineInspectionSchema(**res)
+    #
+    #     except Exception as err:
+    #         print(f"Error found : {err}")
+    #         print(res)
+    #         error_resp = {
+    #             "error": str(err),
+    #             "appointmentId": res["appointmentId"],
+    #             "message": "Schema creation failed for this appointmentID."
+    #         }
+    #         response["schema_validations"].append(error_resp)
+    #     else:
+    #         if obj.appointmentId in appointment_ids:
+    #             print(f'{obj.appointmentId} is duplicate in the current file, ignoring it.')
+    #             error_resp = {
+    #                 "message": f'{obj.appointmentId} is duplicate in the file',
+    #                 "appointmentId": res["appointmentId"],
+    #             }
+    #             response["duplicate_records"].append(error_resp)
+    #             continue
+    #         appointment_ids.append(obj.appointmentId)
+    #         appointment_obj.append(EngineInspectionData(**res))
+    #
+    #
+    # with Session(bind=engine, expire_on_commit=False) as session:
+    #     records = session.query(EngineInspectionData)\
+    #         .with_entities(EngineInspectionData.appointmentId)\
+    #         .filter(EngineInspectionData.appointmentId.in_(appointment_ids))
+    # for record in records:
+    #     try:
+    #         indx = appointment_ids.index(record.appointmentId)
+    #         appointment_ids.pop(indx)
+    #         appointment_obj.pop(indx)
+    #         error_resp = {
+    #             "message": f'{record.appointmentId} is already present in DB.',
+    #             "appointmentId": record.appointmentId
+    #         }
+    #         response["duplicate_records"].append(error_resp)
+    #     except Exception as err:
+    #         print(f"Exception: {err}")
+    # if appointment_obj:
+    #     with Session(bind=engine, expire_on_commit=False) as session:
+    #         session.add_all(appointment_obj)
+    #         try:
+    #             session.commit()
+    #             response["metadata"]["inserted_records"] = len(appointment_obj)
+    #         except Exception as err:
+    #             pass
+    # else:
+    #     print("No records left to insert.")
+    #
+    # return response
