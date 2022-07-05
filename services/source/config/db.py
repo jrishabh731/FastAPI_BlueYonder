3041eb435e8514410962cbde6653e9313157f52b
rvjain <rvjain@parallelwireless.com>
2022-07-05 23:34:07 +0530

    Added handling for db connections

--- services/source/config/db.py
+++ services/source/config/db.py
@@ -1,4 +1,6 @@
 import os
+import logging
+import time
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,9 +11,19 @@ db_password = os.getenv("DB_PASSWORD")
 database = os.getenv("POSTGRES_DB")
 port = os.getenv("DB_PORT")
 db_host = os.getenv("DB_HOST")
+log = logging.getLogger("API_LOG")
 
-engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_host}:{port}/{database}")
 
+def initialize_engine():
+    while True:
+        try:
+            return create_engine(f"postgresql://{db_user}:{db_password}@{db_host}:{port}/{database}")
+        except Exception as err:
+            log.error(f"Exception occured while connecting to db: {err}")
+        time.sleep(10)
+
+
+engine = initialize_engine()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
