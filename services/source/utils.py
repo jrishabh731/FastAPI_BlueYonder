7bed364726cf8271db9373b4864adc07c4dc1ac8
rvjain <rvjain@parallelwireless.com>
2022-07-06 20:12:42 +0530

    Added some changes

--- services/source/utils.py
+++ services/source/utils.py
@@ -6,3 +6,5 @@ class Utils:
     @classmethod
     def csv_to_dict(cls, bytes_stream, delimiter=",", encoding='utf-8-sig'):
         return csv.DictReader(StringIO(str(bytes_stream, encoding)), delimiter=delimiter)
+
+
