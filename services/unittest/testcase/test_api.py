a28fbebccf01c48ba34893e445371a97a3f456e6
rvjain <rvjain@parallelwireless.com>
2022-07-07 17:52:17 +0530

    Added testcase for post scenario

--- services/unittest/testcase/test_api.py
+++ services/unittest/testcase/test_api.py
@@ -1,3 +1,4 @@
+import copy
 import json
 import sys
 from unittest.mock import Mock
@@ -19,6 +20,12 @@ class SessionMocker:
 
     def get(self, *args, **kwargs):
         return get_results
+
+    def add_all(self, *args, **kwargs):
+        pass
+
+    def commit(self, *args, **kwargs):
+        pass
 
     def __enter__(self):
         return self
@@ -44,7 +51,37 @@ def test_post_engine_inspection():
     # When schema is valid
     with open(r"/by/unittest/testdata/valid_post_data.json") as fd:
         full_data = json.load(fd)
+
     obj = EngineInspection(SessionMocker)
     obj.get_all_records_with_filter = Mock(return_value=[])
     results = obj.post_engine_inspection(full_data["valid_scenario"]["input"])
     assert results == full_data["valid_scenario"]["output"]
+
+    # When inspection date is invalid
+    obj = EngineInspection(SessionMocker)
+    obj.get_all_records_with_filter = Mock(return_value=[])
+    results = obj.post_engine_inspection(full_data["invalid_inspection_date"]["input"])
+    assert results == full_data["invalid_inspection_date"]["output"]
+
+    # When month is invalid
+    obj = EngineInspection(SessionMocker)
+    obj.get_all_records_with_filter = Mock(return_value=[])
+    results = obj.post_engine_inspection(full_data["invalid_month"]["input"])
+    assert results == full_data["invalid_month"]["output"]
+
+    # When start time is invalid
+    obj = EngineInspection(SessionMocker)
+    obj.get_all_records_with_filter = Mock(return_value=[])
+    results = obj.post_engine_inspection(full_data["invalid_start_time"]["input"])
+    assert results == full_data["invalid_start_time"]["output"]
+
+    # When record already exists in the db
+    obj = EngineInspection(SessionMocker)
+    dummy_obj = copy.deepcopy(full_data["already_exists_in_db"]["input"][0])
+    dummy_obj["inspectionDate"] = dummy_obj.pop("inspection date", None)
+    dummy_obj["inspectionStartTime"] = dummy_obj.pop("inspection time", "")
+    obj.get_all_records_with_filter = Mock(
+        return_value=[EngineInspectionSchema(**dummy_obj)]
+    )
+    results = obj.post_engine_inspection(full_data["already_exists_in_db"]["input"])
+    assert results == full_data["already_exists_in_db"]["output"]
